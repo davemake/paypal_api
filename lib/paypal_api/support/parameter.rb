e80f82251e17712f417bd645a65188bfb722a884
Matt Handler <matt.handler@gmail.com>
2012-02-25 22:55:51 -0500

    pop out parameters, add hash parameter, allow limits on sequential, allow to_key on sequential, add parameter spec and dummy specs for new feats

---
+++ lib/paypal_api/support/parameter.rb
@@ -0,0 +1,180 @@
+
+module Paypal
+
+	class Api
+
+		class Parameter
+			attr_accessor :value
+
+			def initialize(value)
+				@value = value
+			end
+
+			def parse(anything)
+				@value = anything
+				return @value
+			end
+
+			def parameter_parse(val)
+				if @parameter.class == Class
+					if val.class == @parameter
+						return val
+					else
+						raise Paypal::InvalidParameter, "'#{val}'' is not of type #{@parameter.class}"
+					end
+				elsif @parameter.class == Regexp
+					match = @parameter.match(val)
+					if match
+						return match[0]
+					else
+						raise Paypal::InvalidParameter, "'#{val}' does not match #{@parameter}"
+					end
+				elsif @parameter.class < Parameter
+					return @parameter.parse(val)
+				else
+					raise Paypal::InvalidParameter, "#{@parameter.class} is an invalid parameter specification"
+				end
+			end
+		end
+
+		class Sequential < Parameter
+			include Paypal::Formatters
+
+			attr_accessor :list
+
+			# allows you to specify an optional key -> paypal_key proc due to nonstandard key formatting
+			def initialize(hash = {}, limit = nil, key_proc = nil)
+				@list = []
+				@schema = hash
+				@required = hash.map{|(k,v)| k if v.class != Optional }.compact
+				@key_proc = key_proc if key_proc
+				@limit = limit
+			end
+
+			def push(hash)
+				raise Paypal::InvalidParameter, "missing required parameter for sequential field" unless (@required - hash.keys).empty?
+				raise Paypal::InvalidParameter, "field cannot have more than #{@limit} items" if !@limit.nil? && @list.length == @limit
+
+				hash.each do |k,val|
+					type = @schema[k]
+
+					if type.nil?
+						hash[k] = val
+					elsif type.class == Regexp
+						if match = type.match(val)
+							hash[k] = match[0]
+						else
+							raise Paypal::InvalidParameter, "'#{val}' did not match #{type}"
+						end
+					elsif [Optional, Enum, Coerce, Default].include?(type.class)
+						hash[k] = type.parse(val)
+					elsif type.class == Proc && type.call(val)
+						hash[k] = val
+					elsif type == val.class
+						hash[k] = val
+					else
+						raise Paypal::InvalidParameter, "#{type.class} is an invalid parameter specification"
+					end
+				end
+
+				@list.push(hash)
+			end
+
+			def to_key(symbol, i)
+				if @key_proc
+					return @key_proc.call(symbol, i)
+				else
+					return symbol.to_s.split("_", 2).map{|s| s.gsub("_", "") }.join("_").gsub(/[^a-z0-9_]/i, "").upcase + "#{count}"
+				end
+			end
+
+			def to_s
+				@list.inject(["", 0]) do |(acc, count), item|
+					[acc + item.inject("") do |acc2, (k,v)|
+						"#{acc2}&#{to_key(k, count)}=#{escape_uri_component(item[k])}"
+					end, count + 1]
+				end
+			end
+
+		end
+
+		class Default < Parameter
+
+			def initialize(value, parameter)
+				@value = value
+				@parameter = parameter
+			end
+
+			def parse(val)
+				return parameter_parse(val)
+			end
+
+		end
+
+		class Coerce < Parameter
+
+			attr_reader :method
+
+			def initialize(method)
+				@method = method
+			end
+
+			def parse(val)
+				return @method.call(val)
+			end
+		end
+
+		class Enum < Parameter
+
+			# needs to return the exact string if given instead of symbol
+			attr_reader :allowed_values
+
+			def initialize(*values)
+				if values.length == 1 && values[0].is_a?(Hash)
+					@hash_enum = true
+					@allowed_values = values[0]
+				else
+					@allowed_values = values
+				end
+			end
+
+			def parse(val)
+				if @hash_enum
+					if @allowed_values.include?(val)
+						return @allowed_values[val]
+					else
+						raise Paypal::InvalidParameter, "'#{val}' must be one of #{@allowed_values.keys}"
+					end
+				else
+					if @allowed_values.include?(normalize(val))
+						return normalize(val)
+					else
+						raise Paypal::InvalidParameter, "'#{val}' must be one of #{@allowed_values}"
+					end
+				end
+			end
+
+			def normalize(val)
+				return val if val.class == String
+				return Paypal::Api.symbol_to_camel(val) if val.class == Symbol
+				return nil
+			end
+		end
+
+		class Optional < Parameter
+			def initialize(parameter = nil)
+				@parameter = parameter
+			end
+
+			def parse(val)
+				return parameter_parse(val)
+			end
+		end
+
+		class Hash < Parameter
+
+		end
+
+	end
+
+end
---
+++ spec/support/parameter_spec.rb
@@ -0,0 +1,157 @@
+require "spec_helper"
+
+describe Paypal::Api::Parameter do
+	before do
+		@api = Paypal::Api
+	end
+
+	describe Paypal::Api::Enum do
+		it "should only allow enumerated input" do
+			param = @api::Enum.new("first", "second", "third")
+
+			param.parse("first").should eq("first")
+			param.parse("second").should eq("second")
+			param.parse("third").should eq("third")
+
+			expect {
+				param.parse("anything else")
+			}.to raise_exception(Paypal::InvalidParameter)
+		end
+
+		describe "hash enumerations" do
+			it "should allow symbols to have formatted values as output"
+
+			it "should not respond to symbols that aren't in the hash"
+
+			it "should know it's a hash enumeration"
+		end
+
+		it "should coerce symbols to strings" do
+			param = @api::Enum.new("First", "Second", "ThirdThing")
+
+			param.parse(:first).should eq("First")
+			param.parse(:second).should eq("Second")
+			param.parse(:third_thing).should eq("ThirdThing")
+
+			expect {
+				param.parse(:anything_else)
+			}.to raise_exception(Paypal::InvalidParameter)
+		end
+	end
+
+	describe Paypal::Api::Coerce do
+		it "should allow a well formed coercion" do
+			param = @api::Coerce.new( lambda { |val| return [1, "1", true].include?(val) ? 1 : 0 } )
+
+			param.parse(1).should eq(1)
+			param.parse("1").should eq(1)
+			param.parse(true).should eq(1)
+
+			param.parse(23).should eq(0)
+			param.parse("adfa").should eq(0)
+		end
+	end
+
+	describe Paypal::Api::Sequential do
+		before do
+			class Test < @api
+				set_request_signature :tester, {
+					:sequential => @api::Sequential.new({:l_string => String, :l_fixnum => Fixnum, :l_set_category => Optional.new(String)})
+				}
+			end
+
+			@request = Test.tester
+			@request.sequential.push({:l_string => "sasdf", :l_fixnum => 23, :l_set_category => "asdfasdf"})
+
+
+			expect {
+				@request.sequential.push({:l_string => "sass"})
+			}.to raise_exception Paypal::InvalidParameter
+
+			expect {
+				@request.sequential.push({:l_string => "sass", :l_fixnum => "string"})
+			}.to raise_exception Paypal::InvalidParameter
+		end
+
+		it "should allow nestedly defined params" do
+			@request.sequentials_string.should include("sasdf")
+			@request.sequentials_string.should include("23")
+		end
+
+		it "should allow optional params" do
+			@request.sequential.push({:l_string => "alf", :l_fixnum => 22})
+			@request.sequentials_string.should_not include("L_SETCATEGORY1")
+		end
+
+		it "should number the items" do
+			@request.sequential.push({:l_string => "alf", :l_fixnum => 22})
+			@request.sequentials_string.should include("L_FIXNUM1")
+			@request.sequentials_string.should include("L_SETCATEGORY0")
+		end
+
+		it "should keep the first underscore" do
+			param = @api::Sequential.new({:l_test => String})
+			param.to_key(:l_test).should eq("L_TEST")
+			param.to_key(:l_set_category).should eq("L_SETCATEGORY")
+		end
+
+		it "should allow a list length limit to be set"
+
+		it "should raise an InvalidParameter exception when more than the limit is pushed"
+
+		describe "when a to_key proc is provided" do
+			it "should use the proc to output keys"
+
+			it "should pass the key and iteration to the proc"
+		end
+	end
+
+	describe Paypal::Api::Hash do
+
+	end
+
+	describe Paypal::Api::Optional do
+
+		it "should allow optional enum" do
+			param = @api::Optional.new(@api::Enum.new("test", "tamp"))
+
+			expect {
+				param.parse("tisk")
+			}.to raise_exception(Paypal::InvalidParameter)
+
+			param.parse("test").should eq("test")
+		end
+
+		it "should allow optional regexp" do
+			param = @api::Optional.new(/test/)
+
+			expect {
+				param.parse("tisk")
+			}.to raise_exception(Paypal::InvalidParameter)
+
+			param.parse("test").should eq("test")
+			param.parse(" sdf a test").should eq("test")
+		end
+
+		it "should allow optional class specifiers" do
+			param = @api::Optional.new(String)
+
+			expect {
+				param.parse(2)
+			}.to raise_exception(Paypal::InvalidParameter)
+
+			param.parse("test").should eq("test")
+			param.parse(" sdf a test").should eq(" sdf a test")
+		end
+
+		it "should allow a hash specifier"
+
+		describe "when hash specifier provided" do
+			it "should still be allowed to have optional within"
+
+			it "should validate required fields"
+
+			it "should dot the fields together somehow?"
+		end
+	end
+end
